9fe3002c7e9544f4b15e62d0bf76fd97e49b26ec
Jakub Woźniak <wozniakj708@gmail.com>
2022-08-24 12:27:54 +0200

    Add spinning circle while loading coaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,28 +1,43 @@
 <template>
-  <section><coach-filter @change-filter="setFilters"></coach-filter></section>
-  <section>
-    <base-card>
-      <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
-        <base-button v-if="!isCoach" link to="/register"
-          >Register as Coach</base-button
-        >
-      </div>
-      <ul v-if="hasCoaches">
-        <coach-item
-          v-for="coach in filteredCoaches"
-          :key="coach.id"
-          :id="coach.id"
-          :first-name="coach.firstName"
-          :last-name="coach.lastName"
-          :rate="coach.hourlyRate"
-          :areas="coach.areas"
-        ></coach-item>
-        <!--:key służy tylko do rozroznienia ktory element należy do którego i musi być zawsze uwzgledniony i przypisany najlepiej do ID-->
-      </ul>
-      <h3 v-else>No coaches found</h3>
-    </base-card>
-  </section>
+  <div>
+    <base-dialog
+      :show="!!error"
+      title="An error occurred!"
+      @close="handleError"
+    >
+      <p>{{ error }}</p>
+    </base-dialog>
+    <section>
+      <coach-filter @change-filter="setFilters"></coach-filter>
+    </section>
+    <section>
+      <base-card>
+        <div class="controls">
+          <base-button mode="outline" @click="loadCoaches(true)"
+            >Refresh</base-button
+          >
+          <base-button v-if="!isCoach && !isLoading" link to="/register"
+            >Register as Coach</base-button
+          >
+        </div>
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul v-else-if="hasCoaches">
+          <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+          ></coach-item>
+        </ul>
+        <h3 v-else>No coaches found.</h3>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -36,6 +51,8 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
+      error: null,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -47,9 +64,8 @@ export default {
     isCoach() {
       return this.$store.getters['coaches/isCoach'];
     },
-
     filteredCoaches() {
-      const coaches = this.$store.getters['coaches/coaches']; // first coaches name means namespaced name and second coaches means getters name
+      const coaches = this.$store.getters['coaches/coaches'];
       return coaches.filter((coach) => {
         if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
           return true;
@@ -64,24 +80,35 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
+  },
+  created() {
+    this.loadCoaches();
   },
   methods: {
     setFilters(updatedFilters) {
-      //updatedFilters dajemy tu z powodu dodanie go w pliku CoachFilter.vue jest on w emicie dlatego przechodzi on z dziecka do rodzica
       this.activeFilters = updatedFilters;
+    },
+    async loadCoaches(refresh = false) {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('coaches/loadCoaches', {
+          forceRefresh: refresh,
+        });
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
 </script>
 
-<style>
-body {
-  margin: 0;
-  padding: 0;
-}
-
+<style scoped>
 ul {
   list-style: none;
   margin: 0;
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -10,22 +10,54 @@ export default {
     };
 
     const response = await fetch(
-      `https://vue-http-demo-360d2-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      `https://vue-http-demo-360d2-default-rtdb.firebaseio.com//coaches/${userId}.json`,
       {
         method: 'PUT',
         body: JSON.stringify(coachData),
       }
-    ); //json dodaje sie tylko w przypadku FireBase
+    );
 
     // const responseData = await response.json();
 
     if (!response.ok) {
-      //error
+      // error ...
     }
 
     context.commit('registerCoach', {
       ...coachData,
       id: userId,
     });
+  },
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
+    const response = await fetch(
+      `https://vue-http-demo-360d2-default-rtdb.firebaseio.com//coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   },
 };
