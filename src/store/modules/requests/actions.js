ce288105e19e2aea7e5ab3d3246ebe09f06431e4
Jakub Woźniak <wozniakj708@gmail.com>
2022-08-24 16:56:17 +0200

    error in action.js

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,33 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(), //tworzy losową zmienną
-      coachId: payload.coachId,
+      // id: new Date().toISOString(), //tworzy losową zmienną
       userEmail: payload.email,
       message: payload.message,
     };
+    const response = await fetch(
+      `https://vue-http-demo-360d2-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to send request.'
+      );
+      throw error;
+    }
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit('addRequest', newRequest); //addRequest nazywa sie tak poniewaz taka nazwe podalismy w pliku mutations.js
+  },
+  fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
   },
 };
